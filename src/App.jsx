ac3234d29b25709a59175333dadeaf62c0a04429
Dhruv Arora <dhruv862arora@gmail.com>
2025-04-08 20:52:51 +0530

    Feature: Added Supabase and Clerk for Auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import { createBrowserRouter ,RouterProvider } from "react-router-dom"
 import AppLayout from "./layouts/app-layout"
 import LandingPage from "./pages/landing"
-import JobListing from "./pages/job-listing"
+import JobListing from "./pages/jobListing"
 import JobPage from "./pages/job"
 import PostJob from "./pages/post-job"
 import SavedJobs from "./pages/saved-job"
 import MyJobs from "./pages/my-jobs"
+import Onboarding from "./pages/onboarding"
+import { ThemeProvider } from "@/components/theme-provider"
 
 const router = createBrowserRouter([
   {
@@ -46,7 +48,9 @@ const router = createBrowserRouter([
 
 function App() {
   return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <RouterProvider router={router}/>
+    </ThemeProvider>
   )
 }
 
