5e544f3d20cc27819391dfe2fd662638612db29e
Dhruv Arora <dhruv862arora@gmail.com>
2025-04-09 16:49:17 +0530

    Feature: Made Authenticated Requests and Handled API Jobs

--- src/App.jsx
+++ src/App.jsx
@@ -9,43 +9,67 @@ import SavedJobs from "./pages/saved-job"
 import MyJobs from "./pages/my-jobs"
 import Onboarding from "./pages/onboarding"
 import { ThemeProvider } from "@/components/theme-provider"
+import ProtectedRoute from "./components/protected-route"
 
 const router = createBrowserRouter([
   {
-    element:<AppLayout />,
-    children: [{
-      path:'/',
-      element:<LandingPage />
-    },
-    {
-      path:'/onboarding',
-      element:<Onboarding />
-    },
-    {
-      path:'/jobs',
-      element:<JobListing />
-    },
-    {
-      path:'/job/:id',
-      element:<JobPage />
-    },
-    {
-      path:'/post-job',
-      element:<PostJob />
-    },
-    {
-      path:'/saved-job',
-      element:<SavedJobs />
-    },
-    {
-      path:'/my-jobs',
-      element:<MyJobs />
-    },
-  
-  ]
-  }
-])
-
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
---
+++ src/components/protected-route.jsx
@@ -0,0 +1,37 @@
+// import { Navigate, useLocation } from "react-router-dom";
+// import { useUser } from "@clerk/clerk-react";
+
+
+// const ProtectedRoute = ({childern}) => {
+//     const {isSignedIn,user,isLoaded} = useUser();
+//     const {pathname} = useLocation();
+
+//     if(isLoaded && !isSignedIn && isSignedIn!==undefined){
+//         return <Navigate to= "/?sign-in=true"/>;
+//     }
+
+//   return childern;
+// }
+
+// export default ProtectedRoute
+
+
+import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+
+const ProtectedRoute = ({ children }) => {
+  const { isSignedIn, isLoaded, user } = useUser();
+  const { pathname } = useLocation();
+
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+    return <Navigate to="/?sign-in=true" />;
+  }
+
+  if(user!=undefined && !user?.unsafeMetadata?.role && pathname!='/onboarding'){
+    return <Navigate to= '/onboarding'/>
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
