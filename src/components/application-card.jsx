2321ed102b59884859a9e804681024a7a17fd649
Dhruv Arora <dhruv862arora@gmail.com>
2025-04-28 20:46:58 +0530

    Feat : Added Minor Feat to improve UI

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
-import { Card, CardHeader, CardTitle } from './ui/card'
-import { Download } from 'lucide-react'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
+import { Boxes, BriefcaseBusiness, BriefcaseBusinessIcon, Download, School } from 'lucide-react'
 
 const ApplicationCard = ({application , isCandidate = false}) => {
+
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = application?.resume;
+        link.target = "_blank"
+        link.click();
+    }
+
   return (
     <Card>
         <CardHeader>
             <CardTitle display  = 'flex justify-between font-bold'>
                 {isCandidate ? `${application?.job?.title} at ${application?.job?.company?.name}` : application?.name}
                 <Download size={"18"}
-                className='bg-white text-black rounded-full h-8 w-8 p-1.5 cursor-pointer'/>
+                className='bg-white text-black rounded-full h-8 w-8 p-1.5 cursor-pointer'
+                onClick={handleDownload}
+                />
             </CardTitle>
         </CardHeader>
+
+        <CardContent className="flex flex-col gap-4 flex-1">
+            <div className='flex flex-col md:flex-row justify-between'>
+                <div className='flex gap-2 items-center'>  
+                    <BriefcaseBusinessIcon size={15}/>{application?.experience} Years of Experience
+                </div>
+                <div className='flex gap-2 items-center'>  
+                    <School size={15}/>{application?.education}
+                </div>
+                <div className='flex gap-2 items-center'>  
+                    <Boxes size={15}/>Skills: {application?.skills}
+                </div>
+            </div>
+            <hr />
+        </CardContent>
+        <CardFooter>
+            <span>{new Date(application?.created_at).toLocaleString()}</span>
+            
+        </CardFooter>
     </Card>
   )
 }
