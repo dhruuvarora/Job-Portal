375501b9d3843fdf744d01822a0dfd9db84864cf
Dhruv Arora <dhruv862arora@gmail.com>
2025-05-04 19:35:21 +0530

    Feat : created post-job UI with MD editor intergrated

--- src/pages/post-job.jsx
+++ src/pages/post-job.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Textarea } from "@/components/ui/textarea";
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { z } from 'zod'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { State } from 'country-state-city';
+import useFetch from '@/hooks/use-fetch';
+import { getCompanies } from '@/api/apiCompanies';
+import { useUser } from '@clerk/clerk-react';
+import { BarLoader } from 'react-spinners';
+import { Navigate } from 'react-router-dom';
+import MDEditor from '@uiw/react-md-editor';
+import { Button } from '@/components/ui/button';
 
 const schema = z.object({
   title:z.string().min(1,{message:"Title is required"}),
@@ -15,6 +22,10 @@ const schema = z.object({
 });
 
 const PostJob = () => {
+
+  const {isLoaded , user} = useUser();
+
+
   const {register, control , handleSubmit, formState:{errors}} = useForm({
     defaultValues:{
       location:"",
@@ -24,21 +35,43 @@ const PostJob = () => {
     resolver: zodResolver(schema),
   });
 
+  const {
+    fn:fnCompanies,
+    data : companies, 
+    loading:loadingCompanies
+  } = useFetch(getCompanies);
+
+  useEffect(()=>{
+    if(isLoaded) fnCompanies();
+  },[isLoaded]);
+
+  if(!isLoaded || loadingCompanies){
+    return <BarLoader className='mb-4' width={"100%"} color='#36d7b7'/>
+  }
+
+  // if(user?.unsafeMetadata?.role !== "recruiter"){
+  //   return <Navigate to={"/jobs"} />
+  // }
+
   return (
     <div>
       <h1 className='gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8'>Post a Job
       </h1>
-      <form>
+      <form className='flex flex-col gap-4 p-4 pb-0'>
         <input placeholder='Job Title' {...register("title")} />
         {errors.title && <p className='text-red-500'>{errors.title.message}</p>}
-      </form>
 
       <Textarea placeholder='Job Description' {...register("description")} />
       {errors.description && <p className='text-red-500'>{errors.description.message}</p>}
 
-      <Select 
-      // value={location} 
-      // onValueChange={(value) => setLocation(value)}
+      <div className='flex items-center gap-4'>
+        <Controller
+        name = "location"
+        control={control}
+        render={({ field }) => (
+          <Select 
+      value={field.value} 
+      onValueChange={field.onChange}
       >
           <SelectTrigger>
             <SelectValue placeholder="Filter by Location" />
@@ -55,6 +88,46 @@ const PostJob = () => {
             </SelectGroup>
           </SelectContent>
         </Select>
+        )}
+        />
+
+<Controller
+  name='company_id'
+  control={control}
+  render={({ field }) => (
+    <Select
+      value={field.value}
+      onValueChange={field.onChange}
+    >
+      <SelectTrigger>
+        <SelectValue placeholder="Filter by Company" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {companies?.map(({ name, id }) => (
+            <SelectItem key={id} value={String(id)}>
+              {name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )}
+/>
+        {/* Add Company Drawer */}
+        </div>
+        {errors.location && (<p className='text-red-500'>{errors.location.message}</p>)}
+        {errors.company_id && (<p className='text-red-500'>{errors.company_id.message}</p>)}
+
+        <Controller
+  name='requirements'
+  control={control}
+  render={({ field }) => <MDEditor value={field.value} onChange = {field.onChange}/> }
+  />
+  {errors.requirements && (<p className='text-red-500'>{errors.requirements.message}</p>)}
+      <Button type='submit' className='mt-2' variant={"blue"} size={"lg"}>Submit</Button>
+      
+      </form>
     </div>
   )
 }
